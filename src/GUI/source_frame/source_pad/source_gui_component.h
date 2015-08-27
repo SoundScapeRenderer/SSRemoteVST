5d7fb61db0d7836e9f46072f078405f9793336d1
Florian Willich <florian.willich@tu-berlin.de>
2015-08-27 17:08:26 +0200

    refactured and documented class regarding issue #7

--- src/GUI/source_frame/source_pad/source_gui_component.h
+++ src/GUI/source_frame/source_pad/source_gui_component.h
@@ -22,20 +22,70 @@
 namespace SSR
 {
 
-  class Source_gui_component : public juce::Component
+  /**
+   * This class represents the Source GUI Component.
+   *
+   * The Source GUI Component is graphically represented as a circle similar
+   * to the source GUI component in the SSR.
+   */
+  class Source_gui_component
+      : public juce::Component
   {
 
   public:
+
+    /**
+     * Constructor initialising the radius with 20.0. Sets the size of the
+     * component to 40x40 pixels.
+     */
     Source_gui_component();
+
+    /**
+     * Constructor initialising the radio with width_and_height / 2. Sets the
+     * size of the component to width_and_height x width_and_height pixels.
+     *
+     * @param   width_and_height        The width and height of the GUI
+     *                                  component.
+     */
     Source_gui_component(const unsigned int width_and_height);
+
+    /**
+     * Destructor.
+     */
     ~Source_gui_component();
+
+    /**
+     * Draws the component.
+     *
+     * For more information on this method please read the JUCE documentaion:
+     *
+     * http://learn.juce.com/doc/classComponent.php#a7cf1862f4af5909ea72827898114a182
+     *
+     * @see http://learn.juce.com/doc/classComponent.php#a7cf1862f4af5909ea72827898114a182
+     *
+     * @param   graphics        The graphics context that must be used to do
+     *                          the drawing operations.
+     */
     void paint(juce::Graphics& graphics) override;
+
+    /**
+     * Returns the radius of the source.
+     */
     float get_radius() const;
 
   private:
-    float radius;
-    std::unique_ptr< juce::Point<float> > location;
+
+    /**
+     * Draws the background of the component.
+     */
     void draw_background(juce::Graphics& graphics);
+
+  private:
+
+    /**
+     * Radius of the circle (representing the source).
+     */
+    float radius;
 
   };
 
