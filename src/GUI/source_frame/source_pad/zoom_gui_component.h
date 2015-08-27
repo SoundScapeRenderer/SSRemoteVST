ad51da3ece3df056068f48e84e500321aef98d6b
Florian Willich <florian.willich@tu-berlin.de>
2015-08-27 18:01:14 +0200

    documented class regarding issue #7

--- src/GUI/source_frame/source_pad/zoom_gui_component.h
+++ src/GUI/source_frame/source_pad/zoom_gui_component.h
@@ -46,9 +46,8 @@ namespace SSR
   public:
 
     /**
-     * Constructor initialising
-     *
-     * Fills the zoom_look_up_table with the following values:
+     * Constructor initialising the zoom_factor with 3 and the
+     * zoom_look_up_table with the following values:
      *
      * index: 0 -> value: 0.4
      * index: 1 -> value: 1
@@ -60,13 +59,47 @@ namespace SSR
      * index: 7 -> value: 200
      * index: 8 -> value: 500
      * index: 9 -> value: 2000
+     *
+     * Sets the current_range by looking it up with the current zoom_factor.
+     *
+     * Sets the size of this GUI component to 25x50 pixels.
      */
     Zoom_GUI_component();
 
+    /**
+     * Destructor.
+     */
     ~Zoom_GUI_component();
 
+    /**
+     * This method is called when a button of this GUI component is clicked.
+     *
+     * For more information please read:
+     *
+     * http://learn.juce.com/doc/classButton_1_1Listener.php#a81499cef24b7189cd0d1581fd9dc9e14
+     *
+     * @see http://learn.juce.com/doc/classButton_1_1Listener.php#a81499cef24b7189cd0d1581fd9dc9e14
+     *
+     * If the button that was clicked is the zoom_in_button, the zoom_factor
+     * will be decreased by 1 (if the zoom_factor is already 0 the zoom_factor
+     * will not be decreased).
+     *
+     * If the button that was clicked is the zoom_out_button, the zoom_factor
+     * will be increased by 1 (if the zoom_factor is already 9 the zoom-factor
+     * will not be increased).
+     *
+     * After decreasing or increasing, the current range will be computed and
+     * change message send.
+     *
+     * @param   textButton      The text button that was clicked.
+     */
     void buttonClicked(juce::Button* textButton) override;
 
+    /**
+     * Returns the current range.
+     *
+     * @return the current range.
+     */
     double get_current_range() const;
 
   private:
@@ -93,11 +126,30 @@ namespace SSR
     double look_up_range(const unsigned int zoom_factor);
 
   private:
+
+    /**
+     * The zoom in button.
+     */
     std::unique_ptr<juce::TextButton> zoom_in_button;
+
+    /**
+     * The zoom out button.
+     */
     std::unique_ptr<juce::TextButton> zoom_out_button;
+
+    /**
+     * The zoom factor.
+     */
     unsigned int zoom_factor;
+
+    /**
+     * The current range represented by the zoom factor in meters.
+     */
     double current_range;
 
+    /**
+     * The look up table for the ranges for each zoom factor.
+     */
     std::array<double, 10> zoom_look_up_table;
 
   };
